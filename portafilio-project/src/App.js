aa310eaa35e99fd32905c342caaa934e59034c29
Diego Estrada <diego.estrada1705@gmail.com>
2026-04-06 08:33:43 -0700

    js filess uploaded

--- portafilio-project/src/App.js
+++ portafilio-project/src/App.js
@@ -1,26 +1,48 @@
 import './App.css';
+import img from './assets/img.png';
 
 function App() {
   return (
     <div className="App">
-      <main>
-        <div>
-          <div class="header_container">
-            <div>
-              <h2 class="name_introduction">
-                Hi, My Name Is Diego Estrada
-              </h2>
-            </div>
-            <div class="my_description">
-              <p>Werb Developer <span>•</span> Computer informatic</p>
-            </div>
-          </div>
-          <div>
+      <header>
 
+      </header>
+      <main>
+        <section className="hero">
+          <div className="hero-container">
+            
+            <div className="hero-left">
+              <span className="badge">AVAILABLE FOR HIRE</span>
+              <h1>
+                Hello, I’m <br />
+                <span className="highlight">Diego Estrada.</span>
+              </h1>
+              <p>
+                Building modern web experiences with <strong>code</strong> and{" "}
+                <strong>passion</strong>. 
+              </p>
+            </div>
+
+            <div className="hero-right">
+              <div className="image-card">
+                <img
+                  src={img}
+                  alt="profile"
+                />
+              </div>
+              <div className="experience">
+                <h2>EXITED FOR</h2>
+                <span>A NEW BEGINNEN</span>
+              </div>
+            </div>
           </div>
-        </div>
+        </section>
       </main>
-      
+      <footer>
+        <div>
+
+        </div>
+      </footer>
     </div>
   );
 }
